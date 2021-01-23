922b4404172a77c22b7e647a33b4e345cb25548e
Busra Oguzoglu <busra.oguzoglu@gmail.com>
2021-01-23 21:28:36 +0300

    Grammar updated, new examples added.

--- grammar_converter.py
+++ grammar_converter.py
@@ -16,8 +16,9 @@ class grammar_converter:
         self.CFG = [x.replace("->", "").split() for x in lines]
         self.dict_of_rules = {}
         self.non_terminals = ['S', 'PP', 'VP', 'NP', 'ADJ', 'ADV', 'Q', 'P', 'V', 'N', 'PRO', 'PRO1','PRO2','PRO3',
-                              'NP1', 'NP2', 'NP3', 'SING',
-                              'VP1', 'VP2', 'VP3',
+                              'VP1','VP2','VP3', 'NP1', 'NP2', 'NP3', 'SING', 'VPPAST1', 'VPPAST2', 'VPPAST3',
+                              'PASTNOUN', 'FUTNOUN',
+                              'VPFUT1', 'VPFUT2', 'VPFUT3', 'FUT',
                               'Past1', 'Past2', 'Past3', 'PL',
                               'PosPro1', 'PosPro2', 'PosPro3'] # we will extend this later on
 
